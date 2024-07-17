e264f8d2d05ab5d27430ce5451a10412527a5da5
tngone-akhil <akhil.biju@orderstack.io>
2024-07-17 14:15:20 +0530

    change1

--- functions/CommonFunction.cs
+++ functions/CommonFunction.cs
@@ -12,6 +12,7 @@ public class CommonFunction
     public static void Login()
     {
         Console.WriteLine("enter the Login id");
+        
         string? id = Console.ReadLine();
         Console.WriteLine("enter the password");
         string? password = Console.ReadLine();
